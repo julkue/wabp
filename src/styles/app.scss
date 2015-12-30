e8bb1a3c9be3c1dff0ff72d88decfd5f976bf97c
Julian Motz <me@julianmotz.com>
2015-12-30 10:47:28 +0100

    Copyright update

--- src/styles/app.scss
+++ src/styles/app.scss
@@ -1,6 +1,6 @@
 /******************************************************
  * jmBoilerplate
- * Copyright (c) 2015, Julian Motz
+ * Copyright (c) 2015–2016, Julian Motz
  * For the full copyright and license information, 
  * please view the LICENSE file that was distributed 
  * with this source code.
